aeae99cad044ca3717f61e209d1245b8dadd77d7
hangouh <hugohv10@gmail.com>
2021-06-10 18:15:20 -0500

    FIX: Enviroment vars

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,16 +1,14 @@
-import { Sequelize } from 'sequelize-typescript'
-
-console.log(__dirname + './../model');
+import {Sequelize} from "sequelize-typescript"
 
 const sequelize = new Sequelize({
     logging: console.log,
-    database: 'ts_project',
-    dialect: 'mysql',
-    username: 'root',
-    password: 'admin',
+    dialect: "mysql",
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     models: [__dirname + '/../model'],
-    host: 'local-mysql-57',
-    port: 3306,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "3306"),
     repositoryMode: true
 });
 
